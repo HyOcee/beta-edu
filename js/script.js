c2f88bd12ca03e462af60a14b340883a2e9b3e74
HyOcee <eosemegbe@gmail.com>
2022-02-19 15:06:13 +0100

    typewriter added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 let typeWriter = document.getElementById('typeWriter');
-let txt = 'Make your trip an unforgettable moment together.'; /* The text */
+let txt = 'You wanna become part of our writers team?'; /* The text */
 let typeIndex = 0;
-let speed = 50; /* The speed/duration of the effect in milliseconds */
+let speed = 150; /* The speed/duration of the effect in milliseconds */
 
 function typeWriterFunction() {
   if (typeIndex < txt.length) {
     typeWriter.innerHTML += txt.charAt(typeIndex);
     typeIndex++;
-    setTimeout(typeWriterFunction, speed);
+  } else {
+      typeIndex = 0;
+      typeWriter.innerHTML = ''
   }
 }
 
-typeWriterFunction();
+setInterval(typeWriterFunction, speed);
