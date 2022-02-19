ae23842d78857a3edf8ca09077a768b8f6d3075f
helen <ololadedamilola7@gmail.com>
2022-02-19 08:20:57 -0800

    anime

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let typeWriter = document.getElementById('typeWriter');
 let txt = 'You wanna become part of our writers team?'; /* The text */
 let typeIndex = 0;
-let speed = 150; /* The speed/duration of the effect in milliseconds */
+let speed = 350; /* The speed/duration of the effect in milliseconds */
 
 function typeWriterFunction() {
   if (typeIndex < txt.length) {
@@ -14,3 +14,19 @@ function typeWriterFunction() {
 }
 
 setInterval(typeWriterFunction, speed);
+
+let x = document.getElementById("beta")
+let text = " Library"
+let i = 0
+
+function myFunction(){
+  if (i < text.length) {
+    x.innerHTML += text.charAt(i);
+    i++
+  } else {
+    i = 0;
+    x.innerHTML = 'Beta'
+  }
+}
+
+setInterval(myFunction, speed);
