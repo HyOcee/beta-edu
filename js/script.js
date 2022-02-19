c9888dcd15e1feb8e8250fffcc456269d45e1cc8
Joshuadada <jdbabatunde98@gmail.com>
2022-02-19 16:44:16 +0100

    API section added, comment removed

--- js/script.js
+++ js/script.js
@@ -10,9 +10,9 @@ fetch(url)
         for (let index = 0; index < 8; index++) {
             const element = list[index];
             let holder = document.getElementById('holder')
-            // let img = document.getElementById('img')
+
+            //create elements
             let cards = document.createElement('div')
-            // console.log(element["kind"])
             let imgCard = document.createElement('div')
             let img = document.createElement('img')
             let h3 = document.createElement('h3')
@@ -25,34 +25,10 @@ fetch(url)
             let bookPublishDateHolder = document.createElement('div')
             let bookPublishDateTitle = document.createElement('div')
             let bookPublishDate = document.createElement('div')
-            // let cardContent = document.createElement('div');
-            // cardContent.innerHTML = `<div class="img-card"><img src="images/regular.svg" alt="Regular Car"></div>
-            //     <h3>${element["kind"]}</h3>`
-
-            //     <div class="car-details" id="book-subtitle-holder">
-            //         <div id="book-subtitle-title">Subtitle:</div>
-            //         <div class="bl" id="book-subtitle">Four Seater</div>
-            //     </div>
-
-            //     <div class="car-details" id="book-author-holder">
-            //         <div id="book-author-title">Author(s):</div>
-            //         <div class="bl" id="book-author">White &amp; Black</div>
-            //     </div>
-
-            //     <div class="car-details" id="book-publish-date-holder">
-            //         <div id="book-publish-date-title">Publish Date:</div>
-            //         <div class="bl" id="book-publish-date">&dollar;5,000</div>
-            //     </div>
-
-            //     <button class="bl-btn">Order Ride</button>
-            // `;
-            // title.innerHTML = `${element.name}`
-            // langauge.innerHTML = `${element.language}`
-            // let img = document.createElement('img')
-            // img.src = `${element.image.medium}`
+           
+            //setting id and classes
             cards.setAttribute('class', 'col-4 card')
             imgCard.setAttribute('class', 'img-card')
-
             bookSubtitleHolder.setAttribute('class', 'car-details')
             bookSubtitleHolder.setAttribute('id', 'book-subtitle-holder')
             bookSubtitleTitle.setAttribute('id', 'book-subtitle-title')
@@ -87,9 +63,6 @@ fetch(url)
             bookPublishDateHolder.append(bookPublishDateTitle)
             bookPublishDateHolder.append(bookPublishDate)
 
-
-            console.log(element.volumeInfo.imageLinks.smallThumbnail)
-            // console.log(v)
             imgCard.append(img);
             cards.append(imgCard)
             cards.append(h3)
